dfca716362057bf9b6ad4a3e00ee74b0abc0afa1
pctopgs <46878790+pctopgs@users.noreply.github.com>
2019-12-08 06:49:25 -0600

    The Book Class

---
+++ Library Management System Project/Book.cpp
@@ -0,0 +1,55 @@
+#include "Book.h"
+
+void Book::setTitle(std::string aTitle)
+{
+    ;
+}
+void Book::setAuthor(std::string aAuthor)
+{
+    ;
+}
+void Book::setGenre(std::string aGenre)
+{
+    ;
+}
+void Book::setContent(std::string aContent)
+{
+    ;
+}
+void Book::setYear(int aYear)
+{
+    ;
+}
+void Book::setPages(int aPages)
+{
+    ;
+}
+void Book::setChpt(int aChpt)
+{
+    ;
+}
+
+std::string Book::getTitle()
+{
+    ;
+}
+std::string Book::getAuthor()
+{
+
+}
+std::string Book::getContent()
+{
+
+}
+std::string Book::getGenre()
+{
+
+}
+int Book::getYear()
+{
+
+}
+int Book::getPages()
+{
+    
+}
---
+++ Library Management System Project/Book.h
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+
+class Book
+{
+    std::string title;
+    std::string author;
+    std::string genre;
+    std::string content;
+    int year;
+    int numPages;
+    int numChpt;
+
+    public:
+    void setTitle(std::string);
+    void setAuthor(std::string);
+    void setGenre(std::string);
+    void setContent(std::string);
+    void setYear(int);
+    void setPages(int);
+    void setChpt(int);
+    
+    std::string getTitle();
+    std::string getAuthor();
+    std::string getGenre();
+    std::string getContent();
+    int getYear();
+    int getPages();
+    int getChpt();
+};
